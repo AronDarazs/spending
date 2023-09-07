c160671d1d0aa75e183f011c868c3578e24e9c26
AronDarazs <arondarazs@gmail.com>
2023-09-07 17:16:36 +0200

    final correction

--- tests/SortingComponent.test.jsx
+++ tests/SortingComponent.test.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import SortingComponent from "../src/components/sortingComponent/sortingComponent";
 
 // Mock the props for the component
